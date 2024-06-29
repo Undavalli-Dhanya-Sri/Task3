88538b662d31a1a87d87c54eafa3a2e82938b0a7
Undavalli-Dhanya-Sri <2200032980cseh@gmail.com>
2024-06-29 11:34:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tic-Tac-Toe</h1>
+        <Game />
+      </header>
+    </div>
+  );
+}
+
+export default App;
